ec43588af4ea57d2e4ea542c0ccc49f5650c5e41
Hack Reactor Students <accounts@hackreactor.com>
2015-07-24 15:42:36 -0700

    chat db created

--- server/schema.sql
+++ server/schema.sql
@@ -2,8 +2,20 @@ CREATE DATABASE chat;
 
 USE chat;
 
+CREATE TABLE users (
+  userid mediumint(8) AUTO_INCREMENT,
+  username varchar(255),
+  PRIMARY KEY (userid)
+);
+
 CREATE TABLE messages (
-  /* Describe your table here.*/
+  messageid mediumint(8) AUTO_INCREMENT,
+    userid mediumint(8),
+    message varchar(255),
+    roomname varchar(255),
+    date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (messageid),
+    FOREIGN KEY (userid) REFERENCES users(userid)
 );
 
 /* Create other tables and define schemas for them here! */
